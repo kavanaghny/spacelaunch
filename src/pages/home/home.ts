311f7edd8a67ab69762c8176da4948d48f134e3d
John Kavanagh <john@stellarlink.com>
2017-12-30 09:57:54 -0500

    added home page content links

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
+import { EventPage } from '../event/event';
 import { NavController } from 'ionic-angular';
+import { SpaceportPage } from '../spaceport/spaceport';
+import { VehiclePage } from '../vehicle/vehicle';
 
 @Component({
   selector: 'page-home',
@@ -9,6 +12,18 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  goToEventPage() {
+    this.navCtrl.push(EventPage);
+  }
+
+  goToVehiclePage() {
+    this.navCtrl.push(VehiclePage);
+  }
+
+  goToSpaceportPage() {
+    this.navCtrl.push(SpaceportPage);
   }
 
 }
