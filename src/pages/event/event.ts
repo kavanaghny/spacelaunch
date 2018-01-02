4d15369d4724f68ed56310f72aab93216d4dc87d
John Kavanagh <john@stellarlink.com>
2018-01-01 19:16:33 -0500

    Backand-powered content for Events, Vehicles and Spaceports.

--- src/pages/event/event.ts
+++ src/pages/event/event.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { BackandService } from '@backand/angular2-sdk'
 
 @Component({
   selector: 'page-event',
@@ -8,8 +8,62 @@ import { NavController } from 'ionic-angular';
 })
 export class EventPage {
 
-  constructor(public navCtrl: NavController) {
+  public items:any[] = [];
+  searchQuery: string;
 
+  constructor(public navCtrl: NavController, private backand: BackandService) {
+    this.searchQuery = '';
+    let that = this;
+    this.backand.on("items_updated",
+      (res: any) => {
+        let a = res as any[];
+        let newItem = {};
+        a.forEach((kv)=> newItem[kv.Key] = kv.Value);
+        that.items.unshift(newItem);
+      }
+    );
+  }
+
+  public getItems() {
+   this.backand.object.getList('event')
+    .then((res: any) => {
+      this.items = res.data;
+    },
+    (err: any) => {
+      alert(err.data);
+    });
+  }
+
+  public filterItems() {
+    // set q to the value of the searchbar
+    var q = this.searchQuery;
+
+    // if the value is an empty string don't filter the items
+    if (!q || q.trim() == '') {
+      return;
+    }
+    else{
+        q = q.trim();
+    }
+
+
+    let params = {
+      filter: [
+        this.backand.helpers.filter.create('name', this.backand.helpers.filter.operators.text.contains, q),
+      ],
+    }
+
+    this.backand.object.getList('event', params)
+    .then((res: any) => {
+      this.items = res.data;
+    },
+    (err: any) => {
+      alert(err.data);
+    });
+  }
+
+  ionViewDidLoad() {
+    this.getItems()
   }
 
 }
--- src/pages/vehicle/vehicle.ts
+++ src/pages/vehicle/vehicle.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { BackandService } from '@backand/angular2-sdk'
 
 @Component({
   selector: 'page-vehicle',
@@ -8,8 +8,62 @@ import { NavController } from 'ionic-angular';
 })
 export class VehiclePage {
 
-  constructor(public navCtrl: NavController) {
+  public items:any[] = [];
+  searchQuery: string;
 
+  constructor(public navCtrl: NavController, private backand: BackandService) {
+    this.searchQuery = '';
+    let that = this;
+    this.backand.on("items_updated",
+      (res: any) => {
+        let a = res as any[];
+        let newItem = {};
+        a.forEach((kv)=> newItem[kv.Key] = kv.Value);
+        that.items.unshift(newItem);
+      }
+    );
+  }
+
+  public getItems() {
+   this.backand.object.getList('vehicle')
+    .then((res: any) => {
+      this.items = res.data;
+    },
+    (err: any) => {
+      alert(err.data);
+    });
+  }
+
+  public filterItems() {
+    // set q to the value of the searchbar
+    var q = this.searchQuery;
+
+    // if the value is an empty string don't filter the items
+    if (!q || q.trim() == '') {
+      return;
+    }
+    else{
+        q = q.trim();
+    }
+
+
+    let params = {
+      filter: [
+        this.backand.helpers.filter.create('name', this.backand.helpers.filter.operators.text.contains, q),
+      ],
+    }
+
+    this.backand.object.getList('vehicle', params)
+    .then((res: any) => {
+      this.items = res.data;
+    },
+    (err: any) => {
+      alert(err.data);
+    });
+  }
+
+  ionViewDidLoad() {
+    this.getItems()
   }
 
 }
