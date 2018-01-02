9ebdb8b81b8fab21f81823ccb241d4a0434e8aa3
John Kavanagh <john@stellarlink.com>
2018-01-02 10:58:38 -0500

    Minor formatting and home page Backand stub.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,6 +3,7 @@ import { EventPage } from '../event/event';
 import { NavController } from 'ionic-angular';
 import { SpaceportPage } from '../spaceport/spaceport';
 import { VehiclePage } from '../vehicle/vehicle';
+import { BackandService } from '@backand/angular2-sdk'
 
 @Component({
   selector: 'page-home',
@@ -10,8 +11,58 @@ import { VehiclePage } from '../vehicle/vehicle';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public vehicles:any[] = [];
+  searchQuery: string;
 
+  constructor(public navCtrl: NavController, private backand: BackandService) {
+    this.searchQuery = '';
+    let that = this;
+    this.backand.on("items_updated",
+      (res: any) => {
+        let a = res as any[];
+        let newItem = {};
+        a.forEach((kv)=> newItem[kv.Key] = kv.Value);
+        that.vehicles.unshift(newItem);
+      }
+    );
+  }
+
+  public getVehicles() {
+   this.backand.object.getList('vehicle')
+    .then((res: any) => {
+      this.vehicles = res.data;
+    },
+    (err: any) => {
+      alert(err.data);
+    });
+  }
+
+  public filterItems() {
+    // set q to the value of the searchbar
+    var q = this.searchQuery;
+
+    // if the value is an empty string don't filter the items
+    if (!q || q.trim() == '') {
+      return;
+    }
+    else{
+        q = q.trim();
+    }
+
+
+    let params = {
+      filter: [
+        this.backand.helpers.filter.create('name', this.backand.helpers.filter.operators.text.contains, q),
+      ],
+    }
+
+    this.backand.object.getList('vehicle', params)
+    .then((res: any) => {
+      this.vehicles = res.data;
+    },
+    (err: any) => {
+      alert(err.data);
+    });
   }
 
   goToEventPage() {
@@ -25,5 +76,9 @@ export class HomePage {
   goToSpaceportPage() {
     this.navCtrl.push(SpaceportPage);
   }
+
+//  ionViewDidLoad() {
+//    this.getVehicles()
+//  }
 
 }
